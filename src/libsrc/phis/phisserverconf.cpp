c1868573738f9796962be75bd43b0220546fc09b
Marius Schumacher <mbs@phisketeer.org>
2014-01-30 18:53:56 +0100

    Prepared for embedded server configuration.

---
+++ src/libsrc/phis/phisserverconf.cpp
@@ -0,0 +1,334 @@
+/*
+#    Copyright (C) 2010-2013  Marius B. Schumacher
+#    Copyright (C) 2011-2013  Phisys AG, Switzerland
+#    Copyright (C) 2012-2013  Phisketeer.org team
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <QSettings>
+#include <QFileDialog>
+#include <QTimer>
+#include <QProcess>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QDir>
+#include <QFile>
+#include "phisserverconf.h"
+#include "phiapplication.h"
+#include "phislogwriter.h"
+
+PHISServerConf::PHISServerConf( QWidget *parent )
+    : QDialog( parent, Qt::Sheet )
+{
+    setupUi( this );
+    _configCombo->insertItem( 0, PHI::defaultString() );
+    _virtualTree->setIndentation( 0 );
+    _tab->setCurrentIndex( 0 );
+    resize( 600, 300 );
+
+    _base=phiApp->dataPath();
+
+    QSettings *s=phiApp->serverSettings();
+    s->beginGroup( PHI::defaultString() );
+
+#ifdef Q_OS_WIN
+    int mode=s->value( L1( "StartMode" ), 3 ).toInt();
+    if ( mode==2 ) _winStartupCheck->setChecked( true );
+    else _winStartupCheck->setChecked( false );
+#else
+    _winStartupCheck->hide();
+#endif
+
+    _base=s->value( L1( "BaseDir" ), _base ).toString();
+    _email->setText( s->value( L1( "Admin" ), L1( "admin@localhost" ) ).toString() );
+    _index->setText( s->value( L1( "Index" ), L1( "index.phis,index.html,index.html" ) ).toString() );
+    _baseDir->setText( _base );
+    _mime->setText( s->value( L1( "MimeTypesFile" ), _base+QDir::separator()+QLatin1String( "mimetypes->txt" ) ).toString() );
+
+    _port->setValue( s->value( L1( "ListenerPort" ), 8080 ).toInt() );
+    _interface->setText( s->value( L1( "ListenerIF" ), L1( "Any" ) ).toString() );
+    _keepAlive->setValue( s->value( L1( "KeepAlive" ), 60000 ).toInt() );
+
+    _logDir->setText( s->value( L1( "LogDir" ), _base+QDir::separator()+QLatin1String( "log" ) ).toString() );
+    qint32 filter=s->value( L1( "LogFilter" ), 0 ).value<qint32>();
+    _logCritical->setChecked( filter & PHISLogWriter::Critical );
+    _logWarn->setChecked( filter & PHISLogWriter::Warning );
+    _logError->setChecked( filter & PHISLogWriter::Error );
+    _logDebug->setChecked( filter & PHISLogWriter::Debug );
+    _logTrace->setChecked( filter & PHISLogWriter::Trace );
+    _logUser->setChecked( filter & PHISLogWriter::User );
+
+    _sslBox->setChecked( s->value( L1( "SSLEnabled" ), false ).toBool() );
+    _sslCert->setText( s->value( L1( "SSLCertificate" ) ).toString() );
+    _sslKey->setText( s->value( L1( "SSLPrivateKey" ) ).toString() );
+    _sslInterface->setText( s->value( L1( "SSLListenerIF" ), L1( "Any" ) ).toString() );
+    _sslPort->setValue( s->value( L1( "SSLListenerPort" ), 443 ).toInt() );
+
+    QStringList hosts=s->childGroups();
+    QString host;
+    foreach ( host, hosts ) {
+        s->beginGroup( host );
+        QTreeWidgetItem *it=new QTreeWidgetItem();
+        it->setText( 0, host );
+        it->setText( 1, s->value( L1( "DocumentRoot" ) ).toString() );
+        it->setText( 2, s->value( L1( "TempDir" ) ).toString() );
+        it->setFlags( Qt::ItemIsEditable | Qt::ItemIsEnabled | Qt::ItemIsSelectable );
+        _virtualTree->addTopLevelItem( it );
+        s->endGroup();
+    }
+    s->endGroup(); // default string
+    QSettings *ps=phiApp->settings();
+    restoreGeometry( ps->value( L1( "GeometryServerconf" ) ).toByteArray() );
+    QTimer::singleShot( 0, this, SLOT( slotInit() ) );
+}
+
+PHISServerConf::~PHISServerConf()
+{
+    QSettings *s=phiApp->settings();
+    s->setValue( L1( "GeometryServerconf" ), saveGeometry() );
+    qDebug( "PHISServerConf::~PHISServerConf()" );
+}
+
+void PHISServerConf::slotInit()
+{
+    /*
+    QSettings *s=PHI::globalSettings();
+    if ( !s->isWritable() ) { // always true in Mac OS X
+        QMessageBox::critical( this, tr( "Missing privileges" ), tr( "You must be administrator "
+            "or root to run this application." ), QMessageBox::Ok );
+        reject();
+        return;
+    }
+    _bin=s->value( L1( "BinDir" ), qApp->applicationDirPath() ).toString();
+    if ( !QFile::exists( _bin ) ) _bin=QString();
+    if ( _bin.isEmpty() ) {
+        _bin=QFileDialog::getExistingDirectory( this,
+            tr( "Please select the binary directory of the Phisketeer installation." ),
+            PHI::applicationRoot() );
+        if ( _bin.isEmpty() ) return;
+        s->setValue( L1( "BinDir" ), _bin );
+        s->sync();
+    }
+    slotCheckService();
+    */
+}
+
+void PHISServerConf::slotCheckService()
+{
+    /*
+    int res=PHI::checkService();
+    qDebug( "RES=%d", res );
+    if ( res==1 ) {
+        _stop->setEnabled( true );
+        _interface->setEnabled( false );
+        _port->setEnabled( false );
+        _keepAlive->setEnabled( false );
+        _clearCache->setEnabled( true );
+        _start->setEnabled( false );
+        _icon->setPixmap( QPixmap( L1( ":/gnome/ok" ) ) );
+    } else {
+        _stop->setEnabled( false );
+        _interface->setEnabled( true );
+        _port->setEnabled( true );
+        _keepAlive->setEnabled( true );
+        _clearCache->setEnabled( false );
+        _start->setEnabled( true );
+        _icon->setPixmap( QPixmap( L1( ":/gnome/del" ) ) );
+    }
+    */
+}
+
+void PHISServerConf::accept()
+{
+    save();
+    QDialog::accept();
+}
+
+void PHISServerConf::save()
+{
+    QSettings *s=phiApp->serverSettings();
+    s->beginGroup( PHI::defaultString() );
+    s->setValue( L1( "Admin" ), _email->text() );
+    s->setValue( L1( "Index" ), _index->text() );
+    s->setValue( L1( "BaseDir" ), _baseDir->text() );
+    s->setValue( L1( "MimeTypesFile" ), _mime->text() );
+    s->setValue( L1( "ListenerPort" ), _port->value() );
+    s->setValue( L1( "ListenerIF" ), _interface->text() );
+    s->setValue( L1( "KeepAlive" ), _keepAlive->value() );
+
+#ifdef Q_OS_WIN
+    QSettings set( L1( "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\phis" ), QSettings::NativeFormat );
+    if ( _winStartupCheck->isChecked() ) {
+        s->setValue( L1( "StartMode" ), 2 );
+        set.setValue( L1( "Start" ), 2 );
+    } else {
+        s->setValue( L1( "StartMode" ), 3 );
+        set.setValue( L1( "Start" ), 3 );
+    }
+#endif
+
+    s->setValue( L1( "LogDir" ), _logDir->text() );
+    qint32 filter=0;
+    if ( _logCritical->isChecked() ) filter |= PHISLogWriter::Critical;
+    if ( _logWarn->isChecked() ) filter |= PHISLogWriter::Warning;
+    if ( _logError->isChecked() ) filter |= PHISLogWriter::Error;
+    if ( _logTrace->isChecked() ) filter |= PHISLogWriter::Trace;
+    if ( _logDebug->isChecked() ) filter |= PHISLogWriter::Debug;
+    if ( _logUser->isChecked() ) filter |= PHISLogWriter::User;
+    s->setValue( L1( "LogFilter" ), filter );
+
+    s->setValue( L1( "SSLEnabled" ), _sslBox->isChecked() );
+    s->setValue( L1( "SSLCertificate" ), _sslCert->text() );
+    s->setValue( L1( "SSLPrivateKey" ), _sslKey->text() );
+    s->setValue( L1( "SSLListenerPort" ), _sslPort->value() );
+    s->setValue( L1( "SSLListenerIF" ), _sslInterface->text() );
+
+    QStringList domains=s->childGroups();
+    QString domain;
+    foreach ( domain, domains ) {
+        s->remove( domain );
+    }
+
+    QTreeWidgetItem *it;
+    for ( int i=0; i<_virtualTree->topLevelItemCount(); i++ ) {
+        it=_virtualTree->topLevelItem( i );
+        if ( it->text( 0 ).isEmpty() ) continue;
+        s->beginGroup( it->text( 0 ) );
+        s->setValue( L1( "DocumentRoot" ), it->text( 1 ) );
+        s->setValue( L1( "TempDir" ), it->text( 2 ) );
+        s->endGroup();
+    }
+    s->endGroup();
+}
+
+void PHISServerConf::on__start_clicked()
+{
+    /*
+    QMessageBox::Button res=QMessageBox::warning( this, tr( "Overwrite config" ),
+        tr( "This will store your current configuration." )+QLatin1Char( '\n' )
+        +tr( "Are you sure you want to continue?" ),
+        QMessageBox::Yes | QMessageBox::Abort, QMessageBox::Abort );
+    if ( res==QMessageBox::Abort ) return;
+    save();
+    if ( !PHI::startPhisService() ) {
+        QMessageBox::warning( this, tr( "Error" ), tr( "Could not start the server." ),
+            QMessageBox::Ok );
+        return;
+    }
+    _start->setEnabled( false );
+    _stop->setEnabled( false );
+    QTimer::singleShot( 2000, this, SLOT( slotCheckService() ) );
+    */
+}
+
+void PHISServerConf::on__stop_clicked()
+{
+    /*
+    if ( !PHI::stopPhisService() ) {
+        QMessageBox::warning( this, tr( "Error" ), tr( "Could not stop the server." ),
+            QMessageBox::Ok );
+        return;
+    }
+    _start->setEnabled( false );
+    _stop->setEnabled( false );
+    QTimer::singleShot( 2000, this, SLOT( slotCheckService() ) );
+    */
+}
+
+void PHISServerConf::on__clearCache_clicked()
+{
+    /*
+    if ( !PHI::clearPhisServiceCache() ) {
+        QMessageBox::warning( this, tr( "Error" ), tr( "Could not clear the server cache." ),
+            QMessageBox::Ok );
+    }
+    */
+}
+
+void PHISServerConf::on__baseDirTool_clicked()
+{
+    /*
+    QString file=QFileDialog::getExistingDirectory( this, tr( "Base directory" ), _base );
+    if ( file.isEmpty() ) return;
+    _base=file;
+    _baseDir->setText( file );
+    */
+}
+
+void PHISServerConf::on__mimeTool_clicked()
+{
+    QString file=QFileDialog::getOpenFileName( this, tr( "Mime types file" ), _base );
+    if ( file.isEmpty() ) return;
+    _mime->setText( file );
+}
+
+void PHISServerConf::on__logDirTool_clicked()
+{
+    QString file=QFileDialog::getExistingDirectory( this, tr( "Log directory" ), _logDir->text() );
+    if ( file.isEmpty() ) return;
+    _logDir->setText( file );
+}
+
+void PHISServerConf::on__rootTool_clicked()
+{
+    QString file=QFileDialog::getExistingDirectory( this, tr( "Document root" ), _root->text() );
+    if ( file.isEmpty() ) return;
+    _root->setText( file );
+}
+
+void PHISServerConf::on__tempTool_clicked()
+{
+    QString file=QFileDialog::getExistingDirectory( this, tr( "Temporary directory" ), _temp->text() );
+    if ( file.isEmpty() ) return;
+    _temp->setText( file );
+}
+
+void PHISServerConf::on__sslCertTool_clicked()
+{
+    QString file=QFileDialog::getOpenFileName( this, tr( "SSL certificate file" ), _sslCert->text() );
+    if ( file.isEmpty() ) return;
+    _sslCert->setText( file );
+}
+
+void PHISServerConf::on__sslKeyTool_clicked()
+{
+    QString file=QFileDialog::getOpenFileName( this, tr( "SSL private key file" ), _sslKey->text() );
+    if ( file.isEmpty() ) return;
+    _sslKey->setText( file );
+}
+
+void PHISServerConf::on__addTool_clicked()
+{
+    QTreeWidgetItem *it=new QTreeWidgetItem();
+    it->setText( 0, _host->text() );
+    it->setText( 1, _root->text() );
+    it->setText( 2, _temp->text() );
+    it->setFlags( Qt::ItemIsEditable | Qt::ItemIsEnabled | Qt::ItemIsSelectable );
+    _virtualTree->addTopLevelItem( it );
+}
+
+void PHISServerConf::on__delTool_clicked()
+{
+    QTreeWidgetItem *it=_virtualTree->currentItem();
+    if ( !it ) return;
+    delete it;
+}
+
+void PHISServerConf::on__virtualTree_itemClicked( QTreeWidgetItem *it , int  )
+{
+    if ( !it ) return;
+    _host->setText( it->text( 0 ) );
+    _root->setText( it->text( 1 ) );
+    _temp->setText( it->text( 2 ) );
+}
---
+++ src/libsrc/phis/phisserverconf.h
@@ -0,0 +1,59 @@
+/*
+#    Copyright (C) 2010-2013  Marius B. Schumacher
+#    Copyright (C) 2011-2013  Phisys AG, Switzerland
+#    Copyright (C) 2012-2013  Phisketeer.org team
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef PHISSERVERCONF_H
+#define PHISSERVERCONF_H
+
+#include <QDialog>
+#include <QTreeWidgetItem>
+#include "ui_serverconf.h"
+
+class PHISServerConf : public QDialog, private Ui::ServerConf
+{
+    Q_OBJECT
+
+public:
+    explicit PHISServerConf( QWidget *parent=0 );
+    virtual ~PHISServerConf();
+
+protected slots:
+    virtual void accept();
+    void slotInit();
+    void on__start_clicked();
+    void on__clearCache_clicked();
+    void on__stop_clicked();
+    void on__baseDirTool_clicked();
+    void on__mimeTool_clicked();
+    void on__virtualTree_itemClicked( QTreeWidgetItem*, int col );
+    void on__addTool_clicked();
+    void on__delTool_clicked();
+    void on__logDirTool_clicked();
+    void on__sslKeyTool_clicked();
+    void on__sslCertTool_clicked();
+    void on__tempTool_clicked();
+    void on__rootTool_clicked();
+    void slotCheckService();
+
+protected:
+    void save();
+
+private:
+    QString _bin, _base;
+};
+
+#endif // PHISSERVERCONF_H
