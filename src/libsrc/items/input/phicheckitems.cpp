ad7c5e891990245be3766f8bc646820768d44089
Marius Schumacher <mbs@phisketeer.org>
2014-03-08 12:19:50 +0100

    Fixed .checked() in ServerScript.

--- src/libsrc/items/input/phicheckitems.cpp
+++ src/libsrc/items/input/phicheckitems.cpp
@@ -187,7 +187,7 @@ QScriptValue PHICheckBoxItem::checked( const QScriptValue &v )
     if ( !v.isValid() ) return realChecked();
     if ( v.toBool()==realChecked() ) return self();
     setChecked( v.toBool() );
-    slotChanged();
+    if ( isClientItem() ) slotChanged();
     return self();
 }
 
